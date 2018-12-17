91c2e13bbdb563bad711e2b78005a3d062d54253
Sw3rD <43823636+iiKillerxSG@users.noreply.github.com>
2018-12-17 21:22:14 +0200

    Update elite.js

--- elite.js
+++ elite.js
@@ -46,7 +46,7 @@ client.on("message", (message) => {
 	          const already = new Discord.RichEmbed()
      .setDescription(":x: You can only have \`\`1\`\` ticket in this server! you already have \`\`1\`\`")  
      .setColor("22BF41");
-	if (message.channel.name.startsWith(`${newname}-` + ticketnumber)) return message.channel.send(already);
+	if (!message.channel.name.startsWith(`${newname}-` + ticketnumber)) return message.channel.send(already);
         if (message.guild.channels.exists("name", "ticket-" + ticketnumber)) return message.channel.send(already);  
 	   if(!elite) {
                 message.guild.createChannel("Elite Tickets ♪..", "category");
@@ -108,6 +108,7 @@ client.on("message", (message) => {
                     }) 
                     .then((collected) => {
                         message.delete();
+			message.delete(4000);
                         message.channel.delete();
                     })  
                     .catch(() => {
