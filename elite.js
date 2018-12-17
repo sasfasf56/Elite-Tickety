af7d134fc5994f267d0692b4ac35f3a054522a4a
Sw3rD <43823636+iiKillerxSG@users.noreply.github.com>
2018-12-17 20:46:18 +0200

    Update elite.js

--- elite.js
+++ elite.js
@@ -131,7 +131,7 @@ client.on("message", (message) => {
      .setDescription(`:x: Please only run this command in a ticket channel!`)  
      .setColor("22BF41")
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(d1dx);
-	if (!message.channel.name.startsWith(`$newname`- + ticketnumber)) return message.channel.send(d1dx);
+	if (!message.channel.name.startsWith(`${newname}-` + ticketnumber)) return message.channel.send(d1dx);
 		const usageof = new Discord.RichEmbed()
      .setDescription(`:x: Usage: \`\`-rename <name>\`\``)  
      .setColor("22BF41");
