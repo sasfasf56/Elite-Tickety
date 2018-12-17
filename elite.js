b3514c25b21898906e4afc44c67c089daaf95722
Sw3rD <43823636+iiKillerxSG@users.noreply.github.com>
2018-12-17 16:47:39 +0200

    Update elite.js

--- elite.js
+++ elite.js
@@ -41,11 +41,11 @@ client.on("message", (message) => {
         let elite = message.guild.channels.find("name", "Elite Tickets ♪..");   
 			     const rerole = new Discord.RichEmbed()
      .setDescription(":x: Please first make a role called exactly \`\`Elite Team ♪..\`\`")  
-     .setColor("1CE143");		    
+     .setColor("22BF41");		    
         if (!message.guild.roles.exists("name", "Elite Team ♪..")) return message.channel.send(rerole);
 	          const already = new Discord.RichEmbed()
      .setDescription(":x: You can only have \`\`1\`\` ticket in this server! you already have \`\`1\`\`")  
-     .setColor("1CE143");
+     .setColor("22BF41");
         if (message.guild.channels.exists("name", "ticket-${ticketnumber}" + ticketnumber)) return message.channel.send(already); 
 	   if(!elite) {
                 message.guild.createChannel("Elite Tickets ♪..", "category");
@@ -70,12 +70,12 @@ client.on("message", (message) => {
             });
 		   const d1 = new Discord.RichEmbed()
      .setDescription(`:white_check_mark: Your ticket has been created <#${ticketx.id}>`)  
-     .setColor("1CE143")
+     .setColor("22BF41")
             message.channel.send(d1);
             const nonedear = new Discord.RichEmbed()
      .setDescription(`Dear ${message.author}, \nThank you for reaching out to our support team!\nWe will get back to you as soon as possible\n`)
      .addField(`Subject`, subject)    
-     .setColor("1CE143")
+     .setColor("22BF41")
      .setAuthor(`${message.author.tag}`, message.author.avatarURL)
      .setFooter(client.user.avatarURL , `Elite Tickety v1.0`)
      .setTimestamp();
@@ -89,16 +89,16 @@ client.on("message", (message) => {
   if (message.content.startsWith("-close")) {
        let EliteTeam = message.guild.roles.find("name", "Elite Team ♪..");
 	              	   const d11xxx = new Discord.RichEmbed()
-     .setDescription(":x: You do not have permission for that command! If you believe this is a mistake please add the role called `\`\`Elite Team ♪..\`\`` to yourself.")  
-     .setColor("1CE143");
+     .setDescription(":x: You do not have permission for that command! If you believe this is a mistake please add the role called \`\`Elite Team ♪..\`\` to yourself.")  
+     .setColor("22BF41");
 	  if(!message.member.roles.has(EliteTeam)) return message.channel.send(d11xxx);
 	   	   const d1x = new Discord.RichEmbed()
      .setDescription(`:x: Please only run this command in a ticket channel!`)  
-     .setColor("1CE143")
+     .setColor("22BF41")
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(d1x);
             	   const d11 = new Discord.RichEmbed()
      .setDescription(`Are you sure you want to close this ticket ? The channel will be deleted.\n**Repeat the command to close the ticket.\nYour request will be voided in 20 seconds.`)  
-     .setColor("1CE143");
+     .setColor("22BF41");
         message.channel.send(d11)
             .then((m) => {
                 message.channel.awaitMessages(response => response.content === '-close', {
@@ -112,7 +112,7 @@ client.on("message", (message) => {
                     .catch(() => {
                       const d11x = new Discord.RichEmbed()
      .setDescription(`:x: Ticket close timed out , the ticket was not closed.`)  
-     .setColor("1CE143")
+     .setColor("22BF41")
                         m.edit(d11x).then(m2 => {
                             m2.delete();
                         }, 3000);
