3857c6f2489908bb71c6e90bb4c8e2802e5e826b
Sw3rD <43823636+iiKillerxSG@users.noreply.github.com>
2018-12-31 22:27:28 +0200

    Update elite.js

--- elite.js
+++ elite.js
@@ -61,7 +61,7 @@ if(message.content.toLowerCase().startsWith(prefix + `new`)) {
      .setDescription(":x: You can only have \`\`1\`\` ticket in this server! you already have \`\`1\`\`")  
      .setColor("22BF41");
         if (message.guild.channels.exists("name", "ticket-" + read._number.toString())) return message.channel.send(already);
-	if (message.guild.channels.exists("name", "ticket-" + newname)) return message.channel.send(already);
+	if (message.channel.name.startsWith("ticket-" + newname)) return message.channel.send(already);
         message.guild.createChannel(`ticket-${read._number.toString()}`, "text").then(ticketx => {
             let role = message.guild.roles.find("name", "● Élite » Team");
             let role2 = message.guild.roles.find("name", "@everyone");
@@ -106,7 +106,7 @@ if(message.content.toLowerCase().startsWith(prefix + `new`)) {
      .setDescription(":x: You can only have \`\`1\`\` ticket in this server! you already have \`\`1\`\`")  
      .setColor("22BF41");
         if (message.guild.channels.exists("name", "ticket-" + read._number.toString())) return message.channel.send(already);
-	if (message.guild.channels.exists("name", "ticket-" + newname)) return message.channel.send(already);
+	if (message.channel.name.startsWith("ticket-" + newname)) return message.channel.send(already);
         message.guild.createChannel(`ticket-${read._number.toString()}`, "text").then(ticketx => {
             let role = message.guild.roles.find("name", "● Élite » Team");
             let role2 = message.guild.roles.find("name", "@everyone");
@@ -205,7 +205,7 @@ client.on("message", async message => {
      .setDescription(`:x: Usage: \`\`${prefix}rename <name>\`\``)  
      .setColor("22BF41");
 	if(!newname) return message.channel.send(NOTX1);
-		message.channel.setName(`ticket` + newname);
+		message.channel.setName(`ticket-` + newname);
 		  const D1 = new Discord.RichEmbed()
      .setDescription(`:white_check_mark: This ticket has been renamed to \`\`${newname}\`\``)  
      .setColor("22BF41");
