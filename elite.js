9dc8ea3acd7a2c45583ef672f9c5ae5bc52dc259
Sw3rD <43823636+iiKillerxSG@users.noreply.github.com>
2018-12-31 21:21:11 +0200

    Update elite.js

--- elite.js
+++ elite.js
@@ -190,28 +190,5 @@ client.on("message", async message => {
         }).catch(console.error);
 
 	  }  
-	  
-    if(message.content === `${prefix}close`) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
-  
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`>confirm\`. This will time out in 10 seconds and be cancelled.`)
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === '>confirm', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-              m2.delete();
-          }, 3000);
-        });
-    });
-  }
-  
-});
 
 client.login(process.env.ELITE_TOKEN);
