6ad18412d40b3d73a0af6bba177f0ab0181021f7
Sw3rD <43823636+iiKillerxSG@users.noreply.github.com>
2018-12-31 22:38:33 +0200

    Update elite.js

--- elite.js
+++ elite.js
@@ -59,9 +59,7 @@ if(message.content.toLowerCase().startsWith(prefix + `new`)) {
 	          const already = new Discord.RichEmbed()
      .setDescription(":x: You can only have \`\`1\`\` ticket in this server! you already have \`\`1\`\`")  
      .setColor("22BF41");
-        if (message.guild.channels.exists("name", "ticket-" + read._number.toString())) return message.channel.send(already);
-	if (message.channel.name.startsWith("ticket-")) return message.channel.send(already);
-	if (message.channel.name.startsWith("ticket-" + read._number.toString())) return message.channel.send(already);
+	if (message.guild.channels.exists("name", "ticket-" + read._number)) return message.channel.send(already);
         message.guild.createChannel(`ticket-${read._number.toString()}`, "text").then(ticketx => {
             let role = message.guild.roles.find("name", "● Élite » Team");
             let role2 = message.guild.roles.find("name", "@everyone");
